37dc70f6b56075778d44e5210ab791176495b7aa
Matteo Parrucci <parruc@gmail.com>
2016-07-06 18:04:36 +0200

    working hard on theme

--- src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
+++ src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
@@ -1,81 +1,61 @@
-;(function($){
+require(["jquery", "jquery.countdown", "jquery.jqueryui", "jquery.bxslider", "jquery.flexslider"], function($) {
+  "use strict";
   $(document).ready(function() {
-      ///// Countdown
-      if($.isFunction($.fn.countdown) ){
-          $('#countdown').countdown($('#countdown').data("deadline"), function(event) {
-              $(this).html(event.strftime('%D giorni %H:%M:%S'));
-          });
-      }
-
-    //Select Navigation on Mobile Devices
-      $('.mobilemenu select').change(function(){
-          var url = $(this).val();
-          if (url) {
-              window.location = url;
-          }
-          return false;
-      });
-
-      ///// Masonry
-      if($.isFunction($.fn.isotope) ){
-          $(window).load(function(){
-              var $container = $('.gallery-item');
-              // init
-              $container.isotope({
-                  // options
-                  itemSelector: '.gal',
-                  layoutMode: 'masonry'
-              });
-          });
-      }
-
-      //// Pretty Photo
-      if($.isFunction($.fn.prettyPhoto) ){
-          $(document).ready(function(){
-              $("a[rel^='prettyPhoto']").prettyPhoto({
-                  theme: 'dark_rounded', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
-                  social_tools:false,
-                  show_title: true, /* true/false */
-              });
-          });
-      }
-
-      var pull 		= $('#pull');
-          menu 		= $('#top-menu ul.clearfix');
-          menuHeight	= menu.height();
-
-      $(pull).on('click', function(e) {
-          e.preventDefault();
-          menu.slideToggle();
-      });
-
-      $(window).resize(function(){
-          var w = $(window).width();
-          if(w > 320 && menu.is(':hidden')) {
-              menu.removeAttr('style');
-          }
-      });
-      var pullb 		= $('#pull-blog');
-          menub 		= $('.b-menu ul');
-          menuHeight	= menub.height();
-
-      $(pullb).on('click', function(e) {
-          e.preventDefault();
-          menub.slideToggle();
-      });
-
-      $(window).resize(function(){
-          var w = $(window).width();
-          if(w > 320 && menub.is(':hidden')) {
-              menub.removeAttr('style');
-          }
-      });
-
-    $(window).load(function() {
-        $('.flexslider').flexslider();
+    //// Flexslider
+    $('.flexslider').flexslider();
+    ///// Countdown
+    $('#countdown').countdown($('#countdown').data("deadline"), function(event) {
+        $(this).html(event.strftime('%D giorni %H:%M:%S'));
     });
 
-    $( "#v-tabs, #tab" ).tabs();
+    // Select Navigation on Mobile Devices
+    /*$('.mobilemenu select').change(function() {
+      var url = $(this).val();
+      if (url) {
+        window.location = url;
+      }
+      return false;
+  });*/
+
+    ///// Masonry Isotope
+    /*$(window).load(function() {
+        var $container = $('.gallery-item');
+        // init
+        $container.isotope({
+          // options
+          itemSelector: '.gal',
+          layoutMode: 'masonry'
+        });
+    });*/
+
+    //// Pretty Photo
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+      theme: 'dark_rounded',
+      /* light_rounded / dark_rounded / light_square / dark_square / facebook */
+      social_tools: false,
+      show_title: true,
+      /* true/false */
+    });
+
+    /*var pull = $('#pull');
+    var menu = $('#portal-globalnav');
+    var menuHeight = menu.height();
+
+    $(pull).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle();
+    });
+
+    var pullb = $('#pull-blog');
+    var menub = $('.b-menu ul');
+    var menuHeight = menub.height();
+
+    $(pullb).on('click', function(e) {
+      e.preventDefault();
+      menub.slideToggle();
+  });*/
+
+    $("#v-tabs, #tab").tabs();
     $('.slider1').bxSlider({
       slideWidth: 157,
       minSlides: 2,
@@ -91,4 +71,4 @@
       slideMargin: 0
     });
   });
-})( jQuery );
+});
