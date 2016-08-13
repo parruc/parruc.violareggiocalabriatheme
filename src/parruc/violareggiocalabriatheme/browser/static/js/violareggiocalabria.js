5b6852641d65da22d6dde77e0122ae509ce26c04
Matteo Parrucci <parruc@gmail.com>
2016-08-13 18:56:30 +0200

    trying to destroy the registry

--- src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
+++ src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
@@ -2,9 +2,9 @@ require(["jquery", "jquery.countdown", "jquery.jqueryui", "jquery.bxslider", "jq
   "use strict";
   $(document).ready(function() {
     //// Flexslider
-    $('.template-hp .flexslider').flexslider({controlNav: true,
-                                              directionNav: false,
-                                              controlsContainer:$(".flex-custom-pager")});
+    $('.flexslider').flexslider({controlNav: true,
+                                 directionNav: false,
+                                 controlsContainer:$(".flex-custom-pager")});
     ///// Countdown
     $('#countdown').countdown($('#countdown').data("deadline"), function(event) {
         $(this).html(event.strftime("%D<span class='sup'>G</span>:%H<span class='sup'>H</span>:%M<span class='sup'>M</span>:%S<span class='sup'>S</span>"));
