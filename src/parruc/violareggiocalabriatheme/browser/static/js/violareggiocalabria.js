68217854f4f2f75de7c99bc8f5ab5acbff461506
Matteo Parrucci <parruc@gmail.com>
2016-09-21 01:57:42 +0200

    merged

--- src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
+++ src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
@@ -36,6 +36,7 @@ require(["jquery", "jquery.countdown", "jquery.jqueryui", "jquery.bxslider", "jq
     $('.dropdown-toggle').dropdown();
 
     $("#v-tabs").tabs();
+
     $('.players-slider').bxSlider({
       slideWidth: 199,
       minSlides: 2,
