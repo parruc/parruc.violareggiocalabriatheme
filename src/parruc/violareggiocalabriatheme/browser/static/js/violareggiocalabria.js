9468c863b8e66ad6d4f126b30768a65e4f275dce
Matteo Parrucci <parruc@gmail.com>
2016-09-24 12:14:37 +0200

    js require

--- src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
+++ src/parruc/violareggiocalabriatheme/browser/static/js/violareggiocalabria.js
@@ -1,9 +1,9 @@
-require(["jquery", "bootstrap", "jquery.countdown", "jquery.jqueryui", "jquery.bxslider", "jquery.flexslider", "jquery.prettyphoto", "jquery.matchheight", ], function($) {
+require(["jquery", "plone", "jquery.countdown", "jquery.jqueryui", "jquery.bxslider", "jquery.flexslider", "jquery.prettyphoto", "jquery.matchheight", ], function($) {
   "use strict";
   $(document).ready(function() {
 
     if($("body.userrole-authenticated").length == 0){
-        $("html/head/base").remove();
+        $("html head base").remove();
     }
 
     //// Flexslider
