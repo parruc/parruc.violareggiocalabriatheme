3ce26955e8b852a204599f42acffe3c82a0abdaf
Matteo Parrucci <parruc@gmail.com>
2016-09-24 17:51:49 +0200

    Back to development: 1.0.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='parruc.violareggiocalabriatheme',
-    version='1.0.6',
+    version='1.0.7.dev0',
     description="Viola Reggiocalabria basket club website, theme package",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
